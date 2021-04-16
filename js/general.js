92b0c2d517e460b33c00c470a4766a8d4a86b8d3
Howl Fu <howlfu@gmail.com>
2021-04-16 14:53:49 +0800

    #1 add select dot to template

--- js/general.js
+++ js/general.js
@@ -1,83 +1,26 @@
-const ID_NOTIFY = 'notify';
-const CN_NOTIFY_REPORT = '.notify-status-report';
-const CN_NOTIFY_DATE = '.notify-status-date';
-const CN_NOTIFY_TITLE = '.notify-status-title';
-const CN_NOTIFY_CAL_DATE = '.notify-status-cal-date';
 
-const CN_NOTIFY_STATUS = '.notify-status';
-
-const STAT_NOTIFY_NORMAL = 0;
-const STAT_NOTIFY_ABNORMAL = 1;
-const STAT_NOTIFY_DANGER = 2;
-const STAT_NOTIFY_MANUAL = 3;
-const STAT_NOTIFY_EARTHQUAKE = 4;
-const STAT_NOTIFY_ABNORMAL_MANUAL = 13;
-const STAT_NOTIFY_DANGER_MANUAL = 23;
-
-var notifyStatusClassMap = {};
-notifyStatusClassMap[STAT_NOTIFY_NORMAL] = 'notify-status-normal';
-notifyStatusClassMap[STAT_NOTIFY_ABNORMAL] = 'notify-status-abnormal';
-notifyStatusClassMap[STAT_NOTIFY_DANGER] = 'notify-status-danger';
-notifyStatusClassMap[
-  STAT_NOTIFY_ABNORMAL_MANUAL] = 'notify-status-abnormal-manual';
-notifyStatusClassMap[
-  STAT_NOTIFY_DANGER_MANUAL] = 'notify-status-danger-manual';
-notifyStatusClassMap[
-  STAT_NOTIFY_EARTHQUAKE] = 'notify-status-earthquake';
-
-var _urlVars = null;
-
-function getUrlVars() {
-  var vars = [];
-  var hash;
-  var hashes = window.location.href.slice(
-    window.location.href.indexOf('?') + 1).split('&');
-  for (var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
-    vars.push(hash[0]);
-    vars[hash[0]] = hash[1];
-  }
-  return vars;
-}
-
-function ensureUrlVars() {
-  if (_urlVars == null) {
-    _urlVars = getUrlVars();
-  }
-}
-
-function getUrlVar(key) {
-  ensureUrlVars();
-  return _urlVars[key];
-}
-
-function isNotifyParam() {
-  return getUrlVar(ID_NOTIFY) !== undefined;
-}
-
-function switchNotifyContent(switchClassName) {
-  var notifyParam = getUrlVar(ID_NOTIFY);
-  if (notifyParam !== undefined) {
-    $(CN_NOTIFY_REPORT).show();
-    var cnNormal = notifyStatusClassMap[STAT_NOTIFY_NORMAL];
-    var cnTarget = notifyStatusClassMap[notifyParam];
-    if (notifyParam !== cnNormal) {
-      $(CN_NOTIFY_STATUS).removeClass(cnNormal);
-      $(CN_NOTIFY_STATUS).addClass(cnTarget);
+var timerIndex = 1;
+let sel_id = 'announce-ind-item-sel';
+$(document).ready(function(){
+  var cv_hot = document.getElementById('hot-pie'); 
+  var context = cv_hot.getContext('2d');
+  loopDot(timerIndex);
+   setInterval(() => {
+    timerIndex++;
+    loopDot(timerIndex);
+    if (timerIndex>6) {
+      timerIndex = 1;
     }
-  } else {
-    $(switchClassName).show();
+   }, 5000);
+  console.log( "ready!" );
+});
+
+function loopDot(index) {
+  let dotId = "#ind-" + index;
+  var lastDotId =  "#ind-" + (index - 1);
+  if(index == 1) {
+    lastDotId =  "#ind-" + 6;
   }
-}
-
-function switchNotifyDate() {
-  switchNotifyContent(CN_NOTIFY_DATE);
-}
-
-function switchNotifyTitle() {
-  switchNotifyContent(CN_NOTIFY_TITLE);
-}
-
-function switchNotifyCalDate() {
-  switchNotifyContent(CN_NOTIFY_CAL_DATE);
+  $(dotId).addClass(sel_id);
+  $(lastDotId).removeClass(sel_id)
 }
